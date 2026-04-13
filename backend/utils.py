b1bc0ea9e8fb7a4771cd5eeca7dc5c41e5db9534
Alok Aenugu <aenugualok@gmail.com>
2026-04-13 09:04:18 -0400

    Added require_fields helper function

--- backend/utils.py
+++ backend/utils.py
@@ -1,13 +1,22 @@
-import mimetypes
+import json
 
+#We will use this helper function for sending JSON responses
 def send_json(self, status, data):
-    body = json.dumps(data).encode()
+    body = json.dumps(data).encode("utf-8")
     self.send_response(status)
     self.send_header("Content-Type", "application/json")
-    self.send_header("Content-Length", len(body))
+    self.send_header("Content-Length", str(len(body)))
     self.end_headers()
     self.wfile.write(body)
 
+#We will use this helper function for parsing request bodies
 def parse_body(self):
     length = int(self.headers.get("Content-Length", 0))
-    return json.loads(self.rfile.read(length)) if length else {}
+    if not length:
+        return {}
+    raw = self.rfile.read(length)
+    return json.loads(raw.decode("utf-8"))
+
+#We will use this helper function for validating if the user sent all the proper field
+def require_fields(body, field_names):
+    return [name for name in field_names if not body.get(name)]
