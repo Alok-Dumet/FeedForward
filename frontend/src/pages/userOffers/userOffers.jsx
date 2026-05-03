a7015599be2de751e54986dd8d67ea208049b87a
Alok Aenugu <156627711+Alok-Dumet@users.noreply.github.com>
2026-05-03 11:20:14 -0400

    removed an unused component and created a re-usable constant variable for filters on userRequests/offers page

--- frontend/src/pages/userOffers/userOffers.jsx
+++ frontend/src/pages/userOffers/userOffers.jsx
@@ -2,20 +2,19 @@ import { useLoaderData } from "react-router-dom";
 
 import ListingPageShell from "../../components/listingPageShell.jsx";
 import useListingFilters from "../../hooks/useListingFilters.js";
-
-const FILTERS = ["All listings", "Posted by you", "Accepted by you"];
+import { MY_LISTINGS_FILTERS } from "../../utils/constants.js";
 
 export default function UserOffers() {
   const { items } = useLoaderData();
   const { activeFilter, filteredItems, isFiltering, setActiveFilter } =
-    useListingFilters(items, FILTERS);
+    useListingFilters(items, MY_LISTINGS_FILTERS);
 
   return (
     <ListingPageShell
       eyebrow="My Offers"
       description="Manage your active offers"
       items={filteredItems}
-      filters={FILTERS}
+      filters={MY_LISTINGS_FILTERS}
       activeFilter={activeFilter}
       onFilterChange={setActiveFilter}
       isFiltering={isFiltering}
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,1 @@
+export const MY_LISTINGS_FILTERS = ["All listings", "Posted by you", "Accepted by you"];
